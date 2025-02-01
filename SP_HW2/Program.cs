652aaad1110ffe093f9452bc953cb025ced57c4c
TheKoteAgent <kotikmaxtop@gmail.com>
2025-02-01 18:29:56 +0300

    Добавьте файлы проекта.

---
+++ SP_HW2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SP_HW2
+{
+
+    internal class Program
+    {
+        public static void ShowSymbol(object param)
+        {
+            var threadParams = param as TestClass;
+            if (threadParams == null) return;
+
+            Console.WriteLine("Start работы метода с принимаемыми аргументами\n");
+
+            for (int i = 0; i < threadParams.SymbolCount; i++)
+            {
+                Thread.Sleep(20);
+                Console.Write(threadParams.Symbol.ToString());
+            }
+
+            Console.WriteLine("\nFinish работы метода с принимаемыми аргументами");
+        }
+
+        static void Main(string[] args)
+        {
+            Thread threadOne = new Thread(new ParameterizedThreadStart(ShowSymbol));
+            Thread threadTwo = new Thread(new ParameterizedThreadStart(ShowSymbol));
+            Thread threadThree = new Thread(new ParameterizedThreadStart(ShowSymbol));
+
+            threadOne.Start(new TestClass { Symbol = '*', SymbolCount = 50 });
+            threadTwo.Start(new TestClass { Symbol = '#', SymbolCount = 50 });
+            threadThree.Start(new TestClass { Symbol = '@', SymbolCount = 50 });
+        }   
+    }
+}
---
+++ SP_HW2/TestClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP_HW2
+{
+    internal class TestClass
+    {
+        public object Symbol { get; set; }
+        public int SymbolCount { get; set; }
+    }
+}
